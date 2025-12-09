2b8e98a2e94214df58b56d68e9a9fac104dff651
Wille Johansson <wille@MacBook-Pro-som-tillhor-Wille.local>
2025-12-09 15:22:09 +0100

    fixed errors in code main

--- main.tf
+++ main.tf
@@ -114,20 +114,20 @@ resource "azurerm_virtual_machine" "lab-dev" {
   # delete_data_disks_on_termination = true
 
   storage_image_reference {
-    publisher = "FancyPenis"
+    publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts"
     version   = "latest"
   }
   storage_os_disk {
-    name              = "DiskHelvete"
+    name              = "OSdisk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
   os_profile {
-    computer_name  = "WilleJohansson!"
-    admin_username = "AdminWille"
+    computer_name  = "Wille!"
+    admin_username = "Wille"
     admin_password = "Wille12345!"
   }
   os_profile_linux_config {
